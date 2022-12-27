8c70efa53a6c63b9c3321c65274b05f074f9c2dd
Mateusz Biel <matbiel@gmail.com>
2022-12-27 18:29:10 +0100

    Update sample app

--- sample-app/src/main.py
+++ sample-app/src/main.py
@@ -1,8 +1,24 @@
 from flask import Flask
+import json
+import socket
+import os
+import time
 
 app = Flask(__name__)
+
+ip = socket.gethostbyname(socket.getfqdn())
+started = time.ctime(os.path.getmtime('/proc/1/cmdline'))
 
 
 @app.route("/")
 def hello_world():
     return "Hello"
+
+
+@app.route("/details")
+def details():
+    response = {
+        'started': str(started),
+        'ip': str(ip)
+    }
+    return json.dumps(response)
