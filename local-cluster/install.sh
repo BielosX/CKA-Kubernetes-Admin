8315975faebee812a2543fb328a5325b211cd1d5
Mateusz Biel <matbiel@gmail.com>
2023-01-02 23:05:43 +0100

    Install containerd

--- local-cluster/install.sh
+++ local-cluster/install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-yes | pacman -S avahi
-yes | pacman -S	nss-mdns
+CONTAINERD_VERSION="$1"
+CONTAINERD_URL="https://github.com/containerd/containerd/releases/download/v${CONTAINERD_VERSION}/containerd-${CONTAINERD_VERSION}-linux-amd64.tar.gz"
+wget -nv -O containerd.tar.gz "$CONTAINERD_URL"
+tar Cxzvf /usr/local containerd.tar.gz
+mkdir -p /usr/local/lib/systemd/system
+wget -nv -O /usr/local/lib/systemd/system/containerd.service https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
 
-echo "MulticastDNS=no" >> /etc/systemd/resolved.conf
+RUNC_VERSION="$2"
+wget -nv -O runc.amd64 "https://github.com/opencontainers/runc/releases/download/v${RUNC_VERSION}/runc.amd64"
+install -m 755 runc.amd64 /usr/local/sbin/runc
 
-systemctl restart systemd-resolved.service
-
-systemctl enable avahi-daemon.service
-systemctl start avahi-daemon.service
+systemctl daemon-reload
+systemctl enable containerd.service
+systemctl start containerd.service
