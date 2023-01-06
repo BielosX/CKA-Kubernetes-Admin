03e0467f902452458f1f09815b84c2a86e11edeb
Mateusz Biel <matbiel@gmail.com>
2023-01-06 01:06:54 +0100

    Add node.sh

--- local-cluster/master.sh
+++ local-cluster/master.sh
@@ -4,3 +4,15 @@ kubeadm init
 export KUBECONFIG=/etc/kubernetes/admin.conf
 curl https://docs.projectcalico.org/manifests/calico.yaml -O
 kubectl apply -f calico.yaml
+
+config_path="/vagrant/configs"
+
+if [ -d $config_path ]; then
+  rm -f $config_path/*
+else
+  mkdir -p $config_path
+fi
+
+cp /etc/kubernetes/admin.conf "${config_path}/admin.conf"
+kubeadm token create --print-join-command > "${config_path}/join.sh"
+chmod +x "${config_path}/join.sh"
---
+++ local-cluster/node.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+config_path="/vagrant/configs"
+
+/bin/bash "${config_path}/join.sh" -v
