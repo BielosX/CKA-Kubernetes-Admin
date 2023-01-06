b252c311117437c78076699c32f44f360f2237e4
Mateusz Biel <matbiel@gmail.com>
2023-01-06 12:14:13 +0100

    Join node

--- local-cluster/master.sh
+++ local-cluster/master.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-kubeadm init
+eth1_ip=$(ip -f inet addr show eth1 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p' | head -n 1 | xargs)
+kubeadm init --apiserver-advertise-address="$eth1_ip" --pod-network-cidr="172.16.0.0/16"
 export KUBECONFIG=/etc/kubernetes/admin.conf
 curl https://docs.projectcalico.org/manifests/calico.yaml -O
 kubectl apply -f calico.yaml
 
 config_path="/vagrant/configs"
 
-if [ -d $config_path ]; then
-  rm -f $config_path/*
-else
-  mkdir -p $config_path
-fi
-
 cp /etc/kubernetes/admin.conf "${config_path}/admin.conf"
-kubeadm token create --print-join-command > "${config_path}/join.sh"
+join_command="$(kubeadm token create --print-join-command) -v 5"
+echo "$join_command" > "${config_path}/join.sh"
 chmod +x "${config_path}/join.sh"
