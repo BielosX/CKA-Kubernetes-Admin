ee1611adfb9d613a6ee38cb9f8a57e8d6c0be7f7
Mateusz Biel <matbiel@gmail.com>
2023-05-16 17:42:56 +0200

    Update imperative.sh

--- imperative.sh
+++ imperative.sh
@@ -8,3 +8,8 @@ kubectl run busybox \
   -o yaml -- sh -c 'sleep 10; echo "Hello"' > "$temp_path"
 minikube cp "$temp_path" /etc/kubernetes/manifests/hello.yaml
 rm "$temp_path"
+
+# Expose Redis server created in test-namespace as NodePort service available on 30123
+kubectl create namespace test-namespace
+kubectl run redis --image=redis:7-alpine --port 6379 -n test-namespace
+kubectl create service nodeport redis-service --tcp=6379:6379 --node-port=30123 -n test-namespace
