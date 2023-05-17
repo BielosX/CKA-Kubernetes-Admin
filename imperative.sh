05ec809d579e4a72927bf3348feb782b76dc5334
Mateusz Biel <matbiel@gmail.com>
2023-05-17 09:15:53 +0200

    Update imperative.sh

--- imperative.sh
+++ imperative.sh
@@ -14,7 +14,20 @@ kubectl create namespace test-namespace
 kubectl run redis --image=redis:7-alpine --port 6379 -n test-namespace
 # Random NodePort will be generated
 kubectl expose pod redis -n test-namespace --type NodePort --port 6379 --target-port 6379
-# Setup editor 
+# Setup editor
 export KUBE_EDITOR=nvim
 # Edit service config and change NodePort property to 30123
 kubectl edit service redis -n test-namespace
+
+
+# Deploy two replicas of Nginx version 1.23 then update image to 1.24
+kubectl create deployment nginx --image=nginx:1.23 --replicas=2
+kubectl set image deployment/nginx nginx=nginx:1.24
+
+
+# Get podIPs of all Pods using JSONPATH
+kubectl get pods -o=jsonpath='{.items[*].status.podIP}'
+
+
+# Get podIPs of all Pods using JSONPATH, every IP in separated line
+kubectl get pods -o=jsonpath='{range .items[*]}{.status.podIP}{"\n"}{end}'
